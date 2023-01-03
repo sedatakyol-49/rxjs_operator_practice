e763ccf3df6493d4a96027a9b291b15a78bbcf31
sedat akyol <sedatakyol92@gmail.com>
2023-01-03 23:51:03 +0100

    retry_delay_scan

--- src/app/views/retry-when-delay-scan/retry-when-delay-scan.component.ts
+++ src/app/views/retry-when-delay-scan/retry-when-delay-scan.component.ts
@@ -1,15 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, delay, interval, map, mergeMap, of, retryWhen, scan, Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-retry-when-delay-scan',
   templateUrl: './retry-when-delay-scan.component.html',
-  styleUrls: ['./retry-when-delay-scan.component.sass']
+  styleUrls: ['./retry-when-delay-scan.component.scss']
 })
 export class RetryWhenDelayScanComponent implements OnInit {
 
-  constructor() { }
+  isBusy:boolean=false;
+  dataSubject$=new BehaviorSubject<any>('')
+  data$:any=this.dataSubject$.asObservable();
+
+  scanSubject=new Subject();
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    //Scan
+    // Useful for encapsulating and managing state. Applies an accumulator (or "reducer function") 
+    // to each value from the source after an initial state is established -- either via a seed value
+    //  (second argument), or from the first value from the source.
+    // const scanExamp=this.scanSubject.subscribe(val=>console.log('Accumulated object:',val))
+    // this.scanSubject.next({name:'Joe'});
+    // this.scanSubject.next({age:30})
+    // this.scanSubject.next({favoriteLanguage:'Javascript'})
+  }
+
+  getData(){
+    const data=this.httpClient.get('https://baconipsum.com/api/?type=meat-ad-filter').pipe(
+      retryWhen((error)=>error.pipe(delay(1000),scan((retryCount)=>{
+      console.log(retryCount)
+      if(retryCount>=5){
+        throw error;
+      }
+      else{
+        retryCount=retryCount
+      }
+      return retryCount;
+      },0)))
+    );
+    data.subscribe((res)=>{
+      this.dataSubject$.next(res)
+      this.isBusy=true
+    })
   }
 
 }
